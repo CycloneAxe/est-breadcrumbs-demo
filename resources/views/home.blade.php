2bed05670cfb3e3daa850d647261de431f21bec6
Kelvin <matrixlity@gmail.com>
2016-03-24 09:49:04 +0800

    change footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+    <div class="title">Laravel Breadcrumbs Demo</div>
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             {!! Breadcrumbs::render('blog'); !!}
